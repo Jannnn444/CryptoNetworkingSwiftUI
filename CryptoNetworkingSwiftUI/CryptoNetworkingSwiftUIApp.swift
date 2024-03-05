6bbefe2d58ec70c8753e350c5fd40190e9560233
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-03-06 02:33:04 +0800

    Initial Commit

---
+++ CryptoNetworkingSwiftUI/CryptoNetworkingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoNetworkingSwiftUIApp.swift
+//  CryptoNetworkingSwiftUI
+//
+//  Created by yucian huang on 2024/3/6.
+//
+
+import SwiftUI
+
+@main
+struct CryptoNetworkingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
