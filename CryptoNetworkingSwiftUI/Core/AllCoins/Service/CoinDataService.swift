a19cd3e74f4cdd07681b82d46c76e975c34283a9
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-03-08 08:36:18 +0800

    Added debug errorMessage.

---
+++ CryptoNetworkingSwiftUI/Core/AllCoins/Service/CoinDataService.swift
@@ -0,0 +1,11 @@
+//
+//  CoinDataService.swift
+//  CryptoNetworkingSwiftUI
+//
+//  Created by yucian huang on 2024/3/8.
+//
+
+import Foundation
+
+
+class CoinDataService {
--- CryptoNetworkingSwiftUI/Core/AllCoins/ViewModels/CoinViewModel.swift
+++ CryptoNetworkingSwiftUI/Core/AllCoins/ViewModels/CoinViewModel.swift
@@ -18,36 +18,6 @@ class CoinViewModel: ObservableObject {
     }
     
     
-    func fetchPrice(coin: String) {
-       
-        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
-        guard let url = URL(string: urlString) else { return }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            DispatchQueue.main.sync {
-                
-                if let error = error {
-                    print("DEBUG: Failed with errpr \(error.localizedDescription)")
-                    self.errorMessage = error.localizedDescription
-                    return
-                }
-                
-                guard let data = data else { return }   //make sure we have data first then jasonSeserialKileer
-                guard let jasonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }  //transfer json into dictionary
-                guard let value = jasonObject[coin] as? [String: Double] else {
-                    print("Failed to parse value")
-                    return
-                }
-                guard let price = value["usd"] else { return }
-                
-                self.coin = coin.capitalized
-                self.price = "$\(price)"
-            }
-            
-        }.resume()
-
-        
-    }
 }
 
 
